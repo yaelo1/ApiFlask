a4579be787ca18973d8754e41ef95e5be35c16fb
yaelo1 <somaryael@gmail.com>
2024-10-25 01:09:15 -0600

    random

--- app.py
+++ app.py
@@ -23,19 +23,15 @@ except Exception as e:
 def ausentismo_rango():
     if modelo is None:
         return jsonify({"error": "Modelo no cargado"}), 500
-
-    archivo = request.json.get("archivo")
+    
     start_date = request.json.get("start_date")
     end_date = request.json.get("end_date")
 
-    if not archivo or not start_date or not end_date:
-        return jsonify({"error": "Debe proporcionar 'archivo', 'start_date' y 'end_date' en el cuerpo de la solicitud"}), 400
+    if not start_date or not end_date:
+        return jsonify({"error": "Debe proporcionar 'start_date' y 'end_date' en el cuerpo de la solicitud"}), 400
+    
 
-    archivo_path = os.path.join(UPLOAD_FOLDER, archivo)
-    if not os.path.exists(archivo_path):
-        return jsonify({"error": f"El archivo '{archivo}' no existe"}), 404
-    
-    
+
     
 if __name__ == '__main__':
     app.run(debug=True)
